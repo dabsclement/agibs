c3a1fa121b48900b66a4bd41094245c9c8905b48
Oluwaseun Dabiri <dadsclement@gmail.com>
2020-04-05 12:31:35 +0100

    project setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+// const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const port = 8000;
+
+
+//set ejs and express Layouts
+//app.set('view engine', 'ejs');
+//app.use(expressLayouts);
+
+//const router = require('./app/route');
+//app.use('/', router);
+
+
+// set static files
+//app.use(express.static(__dirname + '/public'));
+ 
+
+app.listen(port, () => console.log(`server started ${port}!`))
